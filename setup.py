529ff6da57b1999c4346d1717a06a1bc79d16d2f
Pratim Bhagat <bhagatpratim21@gmail.com>
2025-08-20 04:36:00 +0530

    add: setup.py proper configurations

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="tradingapi",
-    version="0.1.0",
-    author="Nerve Solutions",
-    description="A Python SDK for Connecting to Mirae API and Streaming API",
+    name="mStock-TradingApi-B",
+    version="0.1.1",
+    author="Mirae Asset Capital Markets Ltd.",
+    description="A Python SDK for Connecting to mStock Trading API and Streaming API",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="",
-    packages=find_packages(),
+    url="https://github.com/MiraeAsset-mStock/pytradingapi-typeB",
+    packages=find_packages(include=["tradingapi_b", "tradingapi_b.*"]),
+    package_dir={"tradingapi_b": "tradingapi_b"},
     install_requires=[
         "autobahn", 
         "requests",
@@ -21,6 +22,13 @@ setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Financial and Insurance Industry",
+        "Natural Language :: English",
+        "Topic :: Office/Business :: Financial :: Investment",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Software Development :: Libraries"
     ],
     python_requires=">=3.11",
 )
